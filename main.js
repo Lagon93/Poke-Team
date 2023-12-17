5ba8383a3767820898acdb035541f9fdb3c3ef35
Lagon <teixgarcia@gmail.com>
2023-12-17 16:57:05 +0100

    estilos y buscador

--- main.js
+++ main.js
@@ -3,46 +3,114 @@ document.addEventListener('DOMContentLoaded', function () {
     const equipoDiv = document.getElementById('equipo');
     const controlesDiv = document.getElementById('controles');
     const maxPokemons = 6;
+    let isAddingPokemon = false;
 
-    // Agrega los event listeners a los botones
-    const agregarBtn = document.getElementById('agregarBtn');
-    const eliminarBtn = document.getElementById('eliminarBtn');
+    const pokemonNames = [];
 
-    agregarBtn.addEventListener('click', agregarPokemonPorNombre);
-    eliminarBtn.addEventListener('click', eliminarPokemon);
-    
-    function agregarPokemonPorNombre() {
-        if (equipoDiv.children.length >= maxPokemons) {
-            alert('Ya has alcanzado el máximo de Pokémon en tu equipo (6).');
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=1000')
+        .then(response => {
+            const results = response.data.results;
+            pokemonNames.push(...results.map(result => result.name));
+        })
+        .catch(error => {
+            console.error('Error al obtener la lista de nombres de Pokémon', error);
+        });
+
+    function agregarPokemonPorNombre(nombrePokemon) {
+        if (isAddingPokemon || equipoDiv.children.length >= maxPokemons) {
             return;
         }
 
-        // Solicita al usuario el nombre del Pokémon a agregar
-        const nombrePokemon = prompt('Ingresa el nombre del Pokémon:');
+        isAddingPokemon = true;
 
-        if (nombrePokemon !== null && nombrePokemon.trim() !== '') {
-            // Realiza una solicitud a la PokeAPI para obtener la información del Pokémon por nombre
-            axios.get(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon.toLowerCase()}/`)
-                .then(response => {
-                    const pokemon = response.data;
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon.toLowerCase()}/`)
+            .then(response => {
+                const pokemon = response.data;
 
-                    // Crea la imagen del Pokémon
-                    const img = document.createElement('img');
-                    img.src = pokemon.sprites.front_default;
-                    img.alt = pokemon.name;
+                const img = document.createElement('img');
+                img.src = pokemon.sprites.front_default;
+                img.alt = pokemon.name;
 
-                    // Añade la imagen del nuevo Pokémon al equipo
-                    equipoDiv.appendChild(img);
-                })
-                .catch(error => {
-                    console.error(`Error al obtener el Pokémon ${nombrePokemon}`, error);
-                });
+                equipoDiv.appendChild(img);
+
+                // Actualiza el dropdown después de agregar
+                actualizarDropdownEliminar();
+            })
+            .catch(error => {
+                console.error(`Error al obtener el Pokémon ${nombrePokemon}`, error);
+            })
+            .finally(() => {
+                isAddingPokemon = false;
+            });
+    }
+
+    function actualizarListaBusqueda(inputValue) {
+        const listaBusqueda = document.getElementById('listaBusqueda');
+
+        // Oculta la lista si no hay nada escrito
+        if (!inputValue) {
+            listaBusqueda.style.display = 'none';
+            return;
+        }
+
+        listaBusqueda.style.display = 'block';
+        listaBusqueda.innerHTML = '';
+
+        const resultadosFiltrados = pokemonNames.filter(name => name.includes(inputValue.toLowerCase()));
+
+        resultadosFiltrados.forEach(result => {
+            const listItem = document.createElement('li');
+            listItem.textContent = result;
+
+            // Agrega un listener al hacer clic en un elemento de la lista
+            listItem.addEventListener('click', function () {
+                agregarPokemonPorNombre(result);
+            });
+
+            listaBusqueda.appendChild(listItem);
+        });
+    }
+
+    function manejarInputBusqueda(event) {
+        const inputValue = event.target.value;
+        actualizarListaBusqueda(inputValue);
+    }
+
+    function actualizarDropdownEliminar() {
+        const eliminarSelect = document.getElementById('eliminarSelect');
+        eliminarSelect.innerHTML = '';
+
+        // Llena el dropdown con nombres de Pokémon en el equipo
+        for (let i = 0; i < equipoDiv.children.length; i++) {
+            const option = document.createElement('option');
+            option.value = equipoDiv.children[i].alt;
+            option.textContent = equipoDiv.children[i].alt;
+            eliminarSelect.appendChild(option);
         }
     }
 
-    function eliminarPokemon() {
-        // Elimina el último hijo del div de equipo (último Pokémon añadido)
-        equipoDiv.removeChild(equipoDiv.lastChild);
-    }
+    // Agrega los event listeners
+    const buscarInput = document.getElementById('buscarInput');
+    const agregarBtn = document.getElementById('agregarBtn');
+    const eliminarBtn = document.getElementById('eliminarBtn');
 
+    buscarInput.addEventListener('input', manejarInputBusqueda);
+    agregarBtn.addEventListener('mousedown', function () {
+        const seleccionado = buscarInput.value.trim();
+        if (seleccionado) {
+            agregarPokemonPorNombre(seleccionado);
+        }
+    });
+
+    eliminarBtn.addEventListener('click', function () {
+        const eliminarSelect = document.getElementById('eliminarSelect');
+        const seleccionado = eliminarSelect.value;
+        if (seleccionado) {
+            equipoDiv.removeChild(equipoDiv.querySelector(`img[alt="${seleccionado}"]`));
+            actualizarDropdownEliminar(); // Actualiza el dropdown después de eliminar
+        }
+    });
+
+    // Llena el dropdown al cargar la página
+    actualizarDropdownEliminar();
 });
